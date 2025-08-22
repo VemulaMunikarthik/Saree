f4e68831b969c527424e37461773966f3759b325
VemulaMunikarthik <148795331+VemulaMunikarthik@users.noreply.github.com>
2025-08-22 15:37:15 +0530

    Delete Desktop/React/Sareeapp directory

--- Desktop/React/Sareeapp/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace Saree with the EXACT name of your GitHub repo!
-export default defineConfig({
-  plugins: [react()],
-  base: '/Saree/',  // ← This is essential for GitHub Pages!
-})
