ec1612aa2d0d75cb6d1a54e03c54f5b980e8727d
VemulaMunikarthik <148795331+VemulaMunikarthik@users.noreply.github.com>
2025-08-22 14:45:23 +0530

    Update vite.config.js

--- Desktop/React/Sareeapp/vite.config.js
+++ Desktop/React/Sareeapp/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace Saree with the EXACT name of your GitHub repo!
 export default defineConfig({
   plugins: [react()],
+  base: '/Saree/',  // ← This is essential for GitHub Pages!
 })
